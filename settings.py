a62a8292091e7e4553d1eae91856181d41acd284
arkodexx <tiartee3@gmail.com>
2026-01-21 19:00:58 +0100

    first commit

---
+++ settings.py
@@ -0,0 +1,94 @@
+# Scrapy settings for POSTscrapy project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = "POSTscrapy"
+
+SPIDER_MODULES = ["POSTscrapy.spiders"]
+NEWSPIDER_MODULE = "POSTscrapy.spiders"
+
+ADDONS = {}
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Concurrency and throttling settings
+# CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+DOWNLOAD_DELAY = 0.5
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+    "Accept": "application/json",
+    "Accept-Language": "de,en-US;q=0.9,en;q=0.8",
+    "Acept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Origin": "https://www.lacoste.com",
+    "Referer": "https://www.lacoste.com/",
+    "User-Agent": USER_AGENT,
+    "Host": "cu0iyshi42-dsn.algolia.net",
+    "Content-Type": "application/x-www-form-urlencoded",
+}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    "POSTscrapy.middlewares.PostscrapySpiderMiddleware": 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    "POSTscrapy.middlewares.PostscrapyDownloaderMiddleware": 543,
+#}
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    "POSTscrapy.pipelines.PostscrapyPipeline": 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Set settings whose default value is deprecated to a future-proof value
+FEED_EXPORT_ENCODING = "utf-8"
---
+++ spiders/crawler.py
@@ -0,0 +1,84 @@
+import json
+from urllib.parse import urlencode
+import scrapy
+from scrapy.http import JsonRequest
+from scrapy.exceptions import CloseSpider
+
+class CrawlerSpider(scrapy.Spider):
+    name = "crawler"
+    allowed_domains = ["www.lacoste.com", "algolia.net", "cu0iyshi42-dsn.algolia.net"]
+
+    QUERIES = ["Sneakers", "Polo", "T-Shirts"] # you can add or remove what you want
+    LANGUAGE = "products_de_de" # Or you can enter products_en_us for example if you have proxies or live in US
+
+    headers = {
+        "Accept": "application/json",
+        "Accept-Language": "de,en-US;q=0.9,en;q=0.8",
+        "Acept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Origin": "https://www.lacoste.com",
+        "Referer": "https://www.lacoste.com/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0",
+        "Host": "cu0iyshi42-dsn.algolia.net",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    url = (
+        "https://cu0iyshi42-dsn.algolia.net/1/indexes/*/queries?"
+        "x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.30.0;JS%20Helper%202.19.0"
+        "&x-algolia-application-id=CU0IYSHI42"
+        "&x-algolia-api-key=7e20c005d05fe8f113d8b7e079bf34a1"
+    )
+
+    params_dict = {
+        "query": QUERIES[0],
+        "hitsPerPage": 32,
+        "page": 0,
+        "analyticsTags": "desktop",
+        "attributesToRetrieve": '["pid","name","colors","imagesHttp","imagesHttps","price","badges","urlMaster","urlSwatchPattern","urlSwatchImg","priceBookId","isCustomizable","categoryLabel","genderLabel","productId","manufacturer","colorId","colorLabel","categoryName","productRestrictions"]',
+        "distinct": "true",
+        "responseFields": '["hits","facets","page","nbPages","query","nbHits","userData"]',
+        "clickAnalytics": "true",
+        "filters": "(priceBookId: lacoste-de-sales OR priceBookId: lacoste-de) AND (price.onlineFrom <= 1768932055458 AND price.onlineTo >= 1768932055458)",
+        "facets": '["categoryLabel","universeLabel","genderLabel","searchColorName","sizes","sleeve"]',
+        "tagFilters": ""
+    }
+
+    def build_json_request(self):
+        payload = {
+            "requests": [
+                {
+                    "indexName": self.LANGUAGE,
+                    "params": urlencode(self.params_dict)
+                }
+            ]
+        }
+        return JsonRequest(url=self.url,  method="POST", data=payload, headers=self.headers,callback=self.parse)
+
+    async def start(self):
+        yield self.build_json_request()
+
+    def parse(self, response):
+        current_query = 0
+        info = json.loads(response.body)
+        items = info["results"][0]["hits"]
+        if not info.get('results')[0].get('hits'):
+            current_query += 1
+            self.params_dict["query"]= self.QUERIES[current_query]
+            self.params_dict["page"] = 0
+
+            # raise CloseSpider('No more products found')
+
+        for item in items:
+            try:
+                price = item["price"]["minDisplayValue"]
+            except:
+                price = "N/A"
+            yield {
+                "title": item["name"],
+                "category": item["categoryLabel"],
+                "price": price,
+                "link": item["urlMaster"],
+            }
+        self.params_dict["page"] = int(self.params_dict.get("page", 0)) + 1
+        yield self.build_json_request()
